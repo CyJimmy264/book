13d3a10cc6bf19910f931a6b24f9cf7aea29c7b7
Maksim Veynberg <mv@cj264.ru>
2022-04-07 18:26:16 +0500

    User with public_key and Devise behavior

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  devise_for :users
+
+  resources :users, only: [:index, :show, :update], shallow: true do
+    resources :messages, only: :create
+  end
+
   # Defines the root path route ("/")
   root "home#index"
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
-  factory(:user) do
+  factory :user do
+    public_key { Faker::String.random }
     email { Faker::Internet.email }
     password { Faker::Internet.password }
   end
