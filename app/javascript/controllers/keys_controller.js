80cfa1cfc5b70fcdfb4a0684eeea36bd71c73af5
Maksim Veynberg <mv@cj264.ru>
2022-05-06 01:50:26 +0500

    Reindent keys_controller.js

--- app/javascript/controllers/keys_controller.js
+++ app/javascript/controllers/keys_controller.js
@@ -3,25 +3,25 @@ import { registerKey } from "models/crypto"
 import * as openpgp from "openpgp"
 
 export default class extends Controller {
-    static targets = ["email", "privateKey", "publicKey"];
+  static targets = ["email", "privateKey", "publicKey"];
 
-    async generate(event) {
-        event.preventDefault();
-        const key = await openpgp.generateKey({
-            curve: "curve25519",
-            userIDs: [{ name: "Anonymous", email: this.emailTarget.value }],
-            passphrase: 'super long and hard to guess secret password',
-            format: 'armored',
-        });
+  async generate(event) {
+    event.preventDefault();
+    const key = await openpgp.generateKey({
+      curve: "curve25519",
+      userIDs: [{ name: "Anonymous", email: this.emailTarget.value }],
+      passphrase: 'super long and hard to guess secret password',
+      format: 'armored',
+    });
 
-        this.privateKeyTarget.value = key.privateKey;
-        this.publicKeyTarget.value = key.publicKey;
+    this.privateKeyTarget.value = key.privateKey;
+    this.publicKeyTarget.value = key.publicKey;
+  }
+
+  async register() {
+    const key = this.privateKeyTarget.value;
+    if (key) {
+      await registerKey(key);
     }
-
-    async register() {
-        const key = this.privateKeyTarget.value;
-        if (key) {
-            await registerKey(key);
-        }
-    }
+  }
 }
