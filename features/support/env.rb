0ccc90bd99ffa6dfb9eba8d377c07b0fd8590cc7
Maksim Veynberg <mv@cj264.ru>
2022-10-18 17:14:47 +0500

    Use SimpleCov

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,6 +5,7 @@
 # files.
 
 require 'cucumber/rails'
+require 'simplecov'
 
 # frozen_string_literal: true
 
@@ -62,3 +63,4 @@ end
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
 Cucumber::Rails::Database.javascript_strategy = :truncation
 
+SimpleCov.coverage_dir 'coverage/cucumber'
